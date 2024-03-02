86a46adebcae144150034ba6c3a2a6eec868bc72
Ahn taeuk <dotory0829@naver.com>
2024-03-02 14:49:28 +0900

    fix#01: 추천로직 에러 픽스 - 장르 선택후 가수 배열 호출 api 사용시 기존에 파라미터 개수에 제한을 두지 않았으나 너무 많아질 경우를 대비하여 slice메서드로 최대 50개까지만 호출될 수 있도록 제한을 두었음"

--- src/api/spotify/artistApi.ts
+++ src/api/spotify/artistApi.ts
@@ -8,12 +8,12 @@ export const getArtistInfo = async (artist_id: any) => {
 		const { artists } = data;
 		return artists;
 	} catch (error) {
-		console.error(error);
+		console.error("getArtisiInfoError:", error);
 	}
 };
 
 /** SPOTIFY DOCS: Get Artist's Top Tracks 참조
- *  아티스트 id로 top track 가져오는 API 단, id는 단수 
+ *  아티스트 id로 top track 가져오는 API 단, id는 단수
  * */
 export const getArtistTopTracks = async (artist_id: any) => {
 	try {
@@ -28,27 +28,28 @@ export const getArtistTopTracks = async (artist_id: any) => {
 	}
 };
 
-
 export const getArtistAlbums = async (artist_id: string) => {
-  try {
-    const response = await baseInstance(`artists/${artist_id}/albums?market=KR`)
-    const data = response.data
-    const albumList = data.items
-    return { albumList }
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
+	try {
+		const response = await baseInstance(
+			`artists/${artist_id}/albums?market=KR`,
+		);
+		const data = response.data;
+		const albumList = data.items;
+		return { albumList };
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+};
 
 export const getRelatedArtists = async (artist_id: string) => {
-  try {
-    const response = await baseInstance(`artists/${artist_id}/related-artists`)
-    const data = response.data
-    const artists = data.artists
-    return { artists }
-  } catch (error) {
-    console.error(error)
-    return error
-  }
-}
+	try {
+		const response = await baseInstance(`artists/${artist_id}/related-artists`);
+		const data = response.data;
+		const artists = data.artists;
+		return { artists };
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+};
